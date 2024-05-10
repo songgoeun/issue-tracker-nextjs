8cdb4a0ed4e3ef1de1caff955076f931c4f349b7
songgoeun <zlxl6445@naver.com>
2024-05-10 15:02:12 +0900

    [Feat] adding navbar component

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -3,18 +3,28 @@ import React from "react";
 import { GiWaveCrest } from "react-icons/gi";
 
 const NavBar: React.FC = () => {
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
   return (
     <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
       <Link href="/">
         <GiWaveCrest size={24} color="#8fa9a5" />
       </Link>
       <ul className="flex space-x-6">
-        <li>
-          <Link href="/">DashBoard</Link>
-        </li>
-        <li>
-          <Link href="/issues">Issues</Link>
-        </li>
+        {links.map((list) => {
+          return (
+            <li className="text-zinc-500" key={list.href}>
+              <Link
+                href={list.href}
+                className="text-zinc-500 hover:text-zinc-800 transition-colors"
+              >
+                {list.label}
+              </Link>
+            </li>
+          );
+        })}
       </ul>
     </nav>
   );
