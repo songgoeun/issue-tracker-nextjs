00dc8a8499e69023645b465a310e5cf136621c74
songgoeun <zlxl6445@naver.com>
2024-05-10 15:25:45 +0900

    [Feat] adding classNames with nav style

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,12 +1,21 @@
+"use client";
+
+import classNames from "classnames";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 import { GiWaveCrest } from "react-icons/gi";
 
 const NavBar: React.FC = () => {
+  // usePathname은 client routing을 위해 사용 되는 것이기 때문에 use client를 사용
+  const currentPath = usePathname();
+  console.log(currentPath);
+
   const links = [
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues" },
   ];
+
   return (
     <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
       <Link href="/">
@@ -18,7 +27,14 @@ const NavBar: React.FC = () => {
             <li className="text-zinc-500" key={list.href}>
               <Link
                 href={list.href}
-                className="text-zinc-500 hover:text-zinc-800 transition-colors"
+                // className={`${
+                //   list.href === currentPath ? "text-zinc-900" : "text-zinc-500"
+                // }  hover:text-zinc-800 transition-colors`}
+                className={classNames({
+                  "text-zinc-900": list.href === currentPath,
+                  "text-zinc-500": list.href !== currentPath,
+                  "hover:text-zinc-800 transition-colors": true,
+                })}
               >
                 {list.label}
               </Link>
---
+++ app/issues/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const IssuesPage: React.FC = () => {
+  return <div>IssuesPage</div>;
+};
+
+export default IssuesPage;
